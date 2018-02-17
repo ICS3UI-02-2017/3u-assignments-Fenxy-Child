38c68f81833d4b7f44c6754e2e70785840a1ef80
Jiho Choi <36271997+Fenxy-Child@users.noreply.github.com>
2018-02-17 17:29:04 -0500

    Update A2Q4.java

--- src/Assignment2/A2Q4.java
+++ src/Assignment2/A2Q4.java
@@ -20,33 +20,33 @@ public class A2Q4 {
         City castle = new City();
         
         //create walls
-        new Wall(castle, 1, 1, Direction.EAST)
-        new Wall(castle, 1, 1, Direction.WEST)
-        new Wall(castle, 1, 1, Direction.SOUTH)
-        new Wall(castle, 1, 1, Direction.NORTH)
-        new Wall(castle, 1, 4, Direction.EAST)
-        new Wall(castle, 1, 4, Direction.WEST)
-        new Wall(castle, 1, 4, Direction.SOUTH)
-        new Wall(castle, 1, 4, Direction.NORTH)
-        new Wall(castle, 4, 1, Direction.EAST)
-        new Wall(castle, 4, 1, Direction.WEST)
-        new Wall(castle, 4, 1, Direction.SOUTH)
-        new Wall(castle, 4, 1, Direction.NORTH)
-        new Wall(castle, 4, 4, Direction.EAST)
-        new Wall(castle, 4, 4, Direction.WEST)
-        new Wall(castle, 4, 4, Direction.SOUTH)
-        new Wall(castle, 4, 4, Direction.NORTH)
-        new Wall(castle, 2, 3, Direction.EAST)
-        new Wall(castle, 2, 2, Direction.WEST)
-        new Wall(castle, 3, 2, Direction.SOUTH)
-        new Wall(castle, 2, 2, Direction.NORTH)
-        new Wall(castle, 3, 3, Direction.EAST)
-        new Wall(castle, 3, 2, Direction.WEST)
-        new Wall(castle, 3, 3, Direction.SOUTH)
-        new Wall(castle, 2, 3, Direction.NORTH)
+        new Wall(castle, 1, 1, Direction.EAST);
+        new Wall(castle, 1, 1, Direction.WEST);
+        new Wall(castle, 1, 1, Direction.SOUTH);
+        new Wall(castle, 1, 1, Direction.NORTH);
+        new Wall(castle, 1, 4, Direction.EAST);
+        new Wall(castle, 1, 4, Direction.WEST);
+        new Wall(castle, 1, 4, Direction.SOUTH);
+        new Wall(castle, 1, 4, Direction.NORTH);
+        new Wall(castle, 4, 1, Direction.EAST);
+        new Wall(castle, 4, 1, Direction.WEST);
+        new Wall(castle, 4, 1, Direction.SOUTH);
+        new Wall(castle, 4, 1, Direction.NORTH);
+        new Wall(castle, 4, 4, Direction.EAST);
+        new Wall(castle, 4, 4, Direction.WEST);
+        new Wall(castle, 4, 4, Direction.SOUTH);
+        new Wall(castle, 4, 4, Direction.NORTH);
+        new Wall(castle, 2, 3, Direction.EAST);
+        new Wall(castle, 2, 2, Direction.WEST);
+        new Wall(castle, 3, 2, Direction.SOUTH);
+        new Wall(castle, 2, 2, Direction.NORTH);
+        new Wall(castle, 3, 3, Direction.EAST);
+        new Wall(castle, 3, 2, Direction.WEST);
+        new Wall(castle, 3, 3, Direction.SOUTH);
+        new Wall(castle, 2, 3, Direction.NORTH);
     
         //create a guard
-        RobotSE guard = new RobotSE(castle, 0,0, Direction.EAST)
+        RobotSE guard = new RobotSE(castle, 0,0, Direction.EAST);
             
         //
             
