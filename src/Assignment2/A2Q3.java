448b7aaa34cc1714cd960a769af8623034b65842
Jiho Choi <36271997+Fenxy-Child@users.noreply.github.com>
2018-02-16 20:38:02 -0500

    Update A2Q3.java

--- src/Assignment2/A2Q3.java
+++ src/Assignment2/A2Q3.java
@@ -15,5 +15,18 @@ public class A2Q3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //create a city
+        City seoul = new City();
+        
+        //create a robot
+        RobotSE karel= new RobotSE(seoul,1,5,Direction.EAST);
+        
+        while(karel.getAvenue()!=0 || karel.getStreet()!=0){
+        if(karel.getAvenue()==0){
+            karel.turnRight();
+        }else if(karel.getStreet()==0){
+            karel.turnLeft();
+        }
     }
 }
