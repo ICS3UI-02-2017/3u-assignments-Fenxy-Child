f2d7edd2dfc38e732564c3337347cb199a397ddd
choij2116 <choij2116@HRH-IC0027320.wrdsb.ca>
2018-02-26 10:25:45 -0500

    initial

--- src/Assignment3/A3Q4.java
+++ src/Assignment3/A3Q4.java
@@ -7,6 +7,7 @@ package Assignment3;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.RobotSE;
+import becker.robots.Wall;
 
 /**
  *
@@ -24,6 +25,39 @@ public class A3Q4 {
         //new robot
         RobotSE karel = new RobotSE(seoul,3,3,Direction.SOUTH);
         
+        //Create 4 squares
+        new Wall(seoul,1,1,Direction.NORTH);
+        new Wall(seoul,1,2,Direction.NORTH);
+        new Wall(seoul,4,1,Direction.NORTH);
+        new Wall(seoul,4,2,Direction.NORTH);
+        new Wall(seoul,1,4,Direction.NORTH);
+        new Wall(seoul,1,5,Direction.NORTH);
+        new Wall(seoul,4,4,Direction.NORTH);
+        new Wall(seoul,4,5,Direction.NORTH);
+        new Wall(seoul,1,1,Direction.WEST);
+        new Wall(seoul,2,1,Direction.WEST);
+        new Wall(seoul,4,1,Direction.WEST);
+        new Wall(seoul,5,1,Direction.WEST);
+        new Wall(seoul,1,4,Direction.WEST);
+        new Wall(seoul,2,4,Direction.WEST);
+        new Wall(seoul,4,4,Direction.WEST);
+        new Wall(seoul,5,4,Direction.WEST);
+        new Wall(seoul,2,1,Direction.SOUTH);
+        new Wall(seoul,2,2,Direction.SOUTH);
+        new Wall(seoul,5,1,Direction.SOUTH);
+        new Wall(seoul,5,2,Direction.SOUTH);
+        new Wall(seoul,2,4,Direction.SOUTH);
+        new Wall(seoul,2,5,Direction.SOUTH);
+        new Wall(seoul,5,4,Direction.SOUTH);
+        new Wall(seoul,5,5,Direction.SOUTH);
+        new Wall(seoul,1,2,Direction.EAST);
+        new Wall(seoul,2,2,Direction.EAST);
+        new Wall(seoul,4,2,Direction.EAST);
+        new Wall(seoul,5,2,Direction.EAST);
+        new Wall(seoul,1,5,Direction.EAST);
+        new Wall(seoul,2,5,Direction.EAST);
+        new Wall(seoul,4,5,Direction.EAST);
+        new Wall(seoul,5,5,Direction.EAST);
         
         
     }
