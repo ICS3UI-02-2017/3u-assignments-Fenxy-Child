703658ad60e58ffc170a2fe603b55a8d56e383cb
choij2116 <choij2116@HRH-IC0027320.wrdsb.ca>
2018-03-01 11:17:20 -0500

    initial

--- src/Assignment4/A4Q4.java
+++ src/Assignment4/A4Q4.java
@@ -4,6 +4,8 @@
  */
 package Assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author choij2116
@@ -14,6 +16,48 @@ public class A4Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ask the cost for food
+        System.out.println("How much does the food for prom cost?");
+        //use a scanner to read info
+        Scanner input = new Scanner(System.in);
+        double food = input.nextDouble();
+        //print out the cost for food
+        System.out.println("$"+food);
+        // ask the cost for DJ
+        System.out.println("How much does the DJ cost?");
+        //use a scanner to read info
+        double DJ = input.nextDouble();
+        //print out the cost for DJ
+        System.out.println("$"+DJ);
+        // ask the cost to rent the hall
+        System.out.println("How much does it cost to rent the hall?");
+        //use a scanner to read info
+        double hall = input.nextDouble();
+        //print out the cost for hall
+        System.out.println("$"+hall);
+        // ask the cost for decoration
+        System.out.println("How much does decorations cost?");
+        //use a scanner to read info
+        double deco = input.nextDouble();
+        //print out the cost for decoration
+        System.out.println("$"+deco);
+        // ask the cost for staff
+        System.out.println("How much does it cost for staff?");
+        //use a scanner to read info
+        double staff = input.nextDouble();
+        //print out the cost for staff
+        System.out.println("$"+staff);
+        // ask the miscellaneous cost
+        System.out.println("How much for miscellaneous costs?");
+        //use a scanner to read info
+        double misc = input.nextDouble();
+        //print out the miscellaneous cost
+        System.out.println("$"+misc);
+        
+        //add all of cost and print out
+        double total = food + DJ + hall + deco + staff + misc;
+        double ticket = total / 35;
+        double tickets = Math.ceil(ticket);
+        System.out.println("The total cost is $" + total + ". You will need to sell " + tickets + " tickets to break even");
     }
 }
--- src/Assignment4/A4Q5.java
+++ src/Assignment4/A4Q5.java
@@ -4,6 +4,8 @@
  */
 package Assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author choij2116
@@ -14,6 +16,55 @@ public class A4Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Ask to answer the name
+        System.out.println("Please enter your name.");
+        //use a scanner to read info
+        Scanner input = new Scanner(System.in);
+        //make a string to store a name
+        String name = input.nextLine();
+        //ask the total marks for the first test
+        System.out.println("What was the first test out of?");
+        //make a string to store the first total marks
+        String firstT = input.nextLine();
+        //ask the your marks for the first test
+        System.out.println("What mark did you get?");
+        // make a string to store your first marks
+        String first = input.nextLine();
+        //ask the total marks for the second test
+        System.out.println("What was the second test out of?");
+        //make a string to store the second total marks
+        String secondT = input.nextLine();
+        //ask the your marks for the second test
+        System.out.println("What mark did you get?");
+        // make a string to store your second marks
+        String second = input.nextLine();
+        //ask the total marks for the third test
+        System.out.println("What was the third test out of?");
+        //make a string to store the third total marks
+        String thirdT = input.nextLine();
+        //ask the your marks for the third test
+        System.out.println("What mark did you get?");
+        // make a string to store your third marks
+        String third = input.nextLine();
+        //ask the total marks for the fourth test
+        System.out.println("What was the fourth test out of?");
+        //make a string to store the fourth total marks
+        String fourthT = input.nextLine();
+        //ask the your marks for the fourth test
+        System.out.println("What mark did you get?");
+        // make a string to store your fourth marks
+        String fourth = input.nextLine();
+        //ask the total marks for the fifth test
+        System.out.println("What was the fifth test out of?");
+        //make a string to store the fifth total marks
+        String fifthT = input.nextLine();
+        //ask the your marks for the fifth test
+        System.out.println("What mark did you get?");
+        // make a string to store your fifth marks
+        String fifth = input.nextLine();
+        
+        //print out the total marks
+        System.out.println("Test score for " + name + ".");
+        
     }
 }
