235d8595d5c2258b110c28da1770816662454251
choij2116 <choij2116@HRH-IC0027320.wrdsb.ca>
2018-03-08 10:52:16 -0500

    initial

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -16,36 +16,39 @@ public class A4Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         //use a scanner to read info
         Scanner input = new Scanner(System.in);
-        //ask to enter sum of dices
-        System.out.println("enter sum of dices.");
-        //dice and current location
-        int dice = input.nextInt();
-        int current = dice + 1;
+
+        int current = 1;
         //loops for the game
-        while(current<100){
-        if(dice>=2&&dice<=12){
-                System.out.println("You are now on square " + current);
-                System.out.println("enter sum of dices.");
-            if(current == 9){
-                current = 34;
-            }else if(current ==40){
-                current = 64;
-            }else if(current ==67){
-                current = 86;
-            }else if(current ==54){
-                current = 19;
-            }else if(current ==90){
-                current = 48;
-            }else if(current ==99){
-                current = 77;
+        while (current < 100) {
+            //ask to enter sum of dices
+            System.out.println("enter sum of dices.");
+            //dice and current location
+            int dice = input.nextInt();
+            
+            current = dice + current;
+            if (dice < 2 || dice > 12) {
+                    System.out.println("You quit.");}
+            if (dice >= 2 && dice <= 12) {
+                if (current == 9) {
+                    current = 34;
+                } else if (current == 40) {
+                    current = 64;
+                } else if (current == 67) {
+                    current = 86;
+                } else if (current == 54) {
+                    current = 19;
+                } else if (current == 90) {
+                    current = 48;
+                } else if (current == 99) {
+                    current = 77;
+                }if (current >= 100) {
+                    System.out.println("you win!");
+                }System.out.println("You are now on square " + current);
             }
-            }
-        }while(current == 100){
-                System.out.println("you win!");
-        }if(dice<2||dice>12){
-            System.out.println("You quit.");
         }
-    }
-}
+        }
+        }
+    
