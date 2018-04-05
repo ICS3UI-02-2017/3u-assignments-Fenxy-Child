96f1c8db87e1660dc78f0a386d83099d5d5a3391
choij2116 <choij2116@HRH-IC0027320.wrdsb.ca>
2018-04-05 11:00:50 -0400

    initial

---
+++ src/example/Arrays.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author choij2116
+ */
+public class Arrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // scanner 
+        Scanner input = new Scanner(System.in);
+        
+        //make an array to store expenses
+        double[] expenses = new double[6];
+        
+        // let the user know to get the costs
+        System.out.println("Please enter the six costs for prom.");
+        //use a for loop for input
+        for (int i = 0; i < expenses.length; i++) {
+            expenses[i] = input.nextDouble();    
+        }
+        
+        // adding all the expenses
+        double sum = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            // get the expense
+            double expense = expenses[i];
+            // add it to the sum
+            sum = sum + expense;
+        }
+        // output
+        System.out.println("prom costs " + sum);
+        // take the sum, divide by 35, always round up
+        double ticketNeeded = Math.ceil(sum/35);
+        System.out.println("Need to sell " + ticketNeeded + " dollars.");
+    }
+}
