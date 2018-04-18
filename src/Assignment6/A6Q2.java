b677c0150e241441d20920b5e8abbe8f56f498c1
Jiho Choi <36271997+Fenxy-Child@users.noreply.github.com>
2018-04-17 21:13:05 -0400

    Update A6Q2.java

--- src/Assignment6/A6Q2.java
+++ src/Assignment6/A6Q2.java
@@ -21,19 +21,24 @@ public class A6Q2 {
         
         // ask the number of people
         System.out.println("How many people did you measured their heights?");
+        // create values of number of people and their heights
         int peop = input.nextInt();
         double[] heights = new double[peop];
+        // ask to enter their heights
         System.out.println("Please enter all of their heights. Please enter in cm.");
         
-        // use a for loop for input
+        // use a for loop to create arrays
         for (int i = 0; i < heights.length; i++) {
             heights[i] = input.nextDouble();
         }
+        // create value called "sum" (total heights)
         double sum = 0;
+        // use a for loop to calculate their total heights
         for (int i = 0; i < heights.length; i++) {
             double height = heights[i];
             sum = sum + height;
         }
+        // calculate the average height
         double aver = sum/peop;
         
         //Print out the average height
