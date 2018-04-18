3be9c17352b52d57dffc7777b5e798c1c4380523
Jiho Choi <36271997+Fenxy-Child@users.noreply.github.com>
2018-04-17 21:10:25 -0400

    Update A6Q1.java

--- src/Assignment6/A6Q1.java
+++ src/Assignment6/A6Q1.java
@@ -21,20 +21,27 @@ public class A6Q1 {
        
         // ask the number of students in class
         System.out.println("How many students in the class?");
+        
+        // number of students and their scores
         int students = input.nextInt();
         double[] score = new double[students]; 
+        // ask to type their marks
         System.out.println("Please enter all the marks");
         
-        // use a for loop for input
+        // use a for loop to create arrays
         for (int i = 0; i < score.length; i++) {
             score[i] = input.nextDouble();
         }
+        //create a value of sum (total scores)
         double sum = 0;
+        // use a for loop to calculate the total scores
         for (int i = 0; i < score.length; i++) {
             double scores = score[i];
             sum = sum + scores;
         }
+        // calculate the class average
         double average = sum/students;
+        // print out the class average
         System.out.println("The class average is " + (Math.round(average * 100)/100.0));
     }
 }
