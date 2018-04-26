5be14d0183a8edecd097d28303da89c79e9d8f97
Jiho Choi <36271997+Fenxy-Child@users.noreply.github.com>
2018-04-25 20:55:31 -0400

    Update A6Q5.java

--- src/Assignment6/A6Q5.java
+++ src/Assignment6/A6Q5.java
@@ -14,6 +14,41 @@ public class A6Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Scanner
+        Scanner input = new Scanner(System.in);
+        
+        // ask to type the number of marks
+        System.out.println("Please enter the number of marks.");
+        
+        // number of marks
+        int students = input.nextInt();
+        
+        // create array
+        double [] marks = new double[students];
+        
+        // ask to type all the marks
+        System.out.println("Please enter all the marks.");
+        
+        // use a for loop to swap and order from lowest to highest
+        for (int i = 0; i <= marks.length; i++) {
+            // set marks[i]
+            marks[i] = input.nextInt();
+            for (int j =1 ; j < (n-i); j++){
+                // if statement and swap
+            if(marks[j-1]>marks[j]){
+                int temp = marks[j-1];
+                marks[j-1] = marks[j];
+                marks[j] = temp;
+            }
+            }
+            }
+        
+        // use a for loop to print out in ascending order
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i] + " , ");
+        }
+        
+        int a = Math.round(students/2)
+        System.out.println("The median mark is " + marks[a] + ".")
     }
 }
