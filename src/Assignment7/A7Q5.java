8d088d36c1d3eb3065b8ad0c124d9efa4276e294
choij2116 <choij2116@HRH-IC0027320.wrdsb.ca>
2018-04-27 11:08:50 -0400

    04/27

--- src/Assignment7/A7Q5.java
+++ src/Assignment7/A7Q5.java
@@ -4,6 +4,8 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author choij2116
@@ -14,10 +16,27 @@ public class A7Q5 {
      * @param args the command line arguments
      */
     
-    
+    public static int chaotic(int n, int a){
+         int randNum = (int)(Math.random()*(n - a + 1))+ a;
+         for (n = 0; n <= randNum; n++) {
+             System.out.println("*");
+            }
+         return randNum;
+        }
     
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Scanner 
+        Scanner input = new Scanner(System.in);
+        
+        // ask to type the number of asterisks and rows
+        System.out.println("Please enter the number of total asterisks and rows.");
+        
+        // integer
+        int d = input.nextInt();
+        int b = input.nextInt();
+        
+        chaotic(d, b);
+        
     }
 }
